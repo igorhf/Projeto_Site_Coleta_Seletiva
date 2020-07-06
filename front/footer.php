25b5c7988202fa035f0d639dff11b4e2366294b7
igorhf <igorhf.2@gmail.com>
2020-07-06 11:57:55 -0300

    [Nº14] criando rodape

--- front/footer.php
+++ front/footer.php
@@ -1,3 +1,24 @@
+<hr>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6" style="text-align: justify;">
+            Nosso Objetivo ?<br>
+            O objetivo da Reciclagem tem fim lucrativo, porém temos como grande incentivador desta empreitada as ações sociais direcionadas à ONG´s, com as quais trabalhamos. Além do fator sócio-ambiental, que é o de prestar serviço ao nosso Planeta, evitando mais desmatamentos e reduzindo a quantidade de lixo nas ruas e aterros sanitários, conseqüentemente favorecendo o equilíbrio ambiental. Sendo assim, o nosso setor ainda contribui para geração de muitos empregos diretos e indiretos.
+        </div>
+        <div class="col">
+            <p>Rede Social</p>
+            <img src="assets/img/face.png" width="50" height="50" alt=""><br>
+            <img src="assets/img/whatsapp.png" width="50" height="50" alt=""> (81) 90000-0000
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-md-12">
+            Desenvolvidor por Igor Henrique e Celio Rocha. <?= date("Y") ?>
+        </div>
+    </div>
+</div>
+
 </div>
 
 </body>
